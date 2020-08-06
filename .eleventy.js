c76e09eb26a84ed92f7b75ff66ca9720c4ffc163
Tanner <dolb.tanner@gmail.com>
2020-08-05 21:22:30 -0700

    Create resize sharp function for small images

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,30 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const directory = "./images/";
+
 module.exports = (eleventyConfig) => {
+   
     eleventyConfig.setTemplateFormats([
         "md"
     ]);
 
+    markdownTemplateEngine: "njk";
+
     // Using manual passthrough file copy to include /images and /css in _site output
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("css");
+    
 
-    // Using Nunjucks for templating
-    markdownTemplateEngine: "njk";
-
-
-    // shortcode for increasing text size inside of markdown files
-    eleventyConfig.addPairedShortcode('resize', (content, size) => {
-        return `<span style="font-size: ${size}">${content}</span>`;
-    });
+    // Resize and create a new file for all image files in the directory to sharp small (320w, 240w)s
+    let resizeAllSmall = () => {
+        fs.readdirSync(directory).forEach(file => {
+            const n = 4;
+            let clean = file.slice(0, file.length - n);
+            sharp(`${directory}/${file}`, {failOnError: false})
+                .resize(320, 240)
+                .toFile(`${directory}/${clean}-small.jpg`);
+        });
+    }
+    // resizeAllSmall();    
     
 };
