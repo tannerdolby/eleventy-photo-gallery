787a96a210649106e0d8b44dd69aa5094591e4ac
Tanner <dolb.tanner@gmail.com>
2020-08-05 22:37:57 -0700

    create sharp functions for small,med,large images

--- .eleventy.js
+++ .eleventy.js
@@ -14,17 +14,30 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("css");
     
-
-    // Resize and create a new file for all image files in the directory to sharp small (320w, 240w)s
-    let resizeAllSmall = () => {
-        fs.readdirSync(directory).forEach(file => {
-            const n = 4;
-            let clean = file.slice(0, file.length - n);
-            sharp(`${directory}/${file}`, {failOnError: false})
+    let resizeImgSmall = (fileName) => {
+        let shortName = fileName.slice(0, fileName.length - 4);
+        fs.readFileSync(fileName)
+            sharp(`${fileName}`, { failOnError: false })
                 .resize(320, 240)
-                .toFile(`${directory}/${clean}-small.jpg`);
-        });
-    }
-    // resizeAllSmall();    
-    
+                .toFile(`${shortName}-small.jpg`);
+    };
+    // resizeImgSmall("./images/shop-plants.jpg");
+
+    let resizeImgMed = (fileName) => {
+        let shortName = fileName.slice(0, fileName.length - 4);
+        fs.readFileSync(fileName)
+            sharp(`${fileName}`, { failOnError: false })
+                .resize(640, 480)
+                .toFile(`${shortName}-med.jpg`);
+    };
+    // resizeImgMed("./images/shop-plants.jpg");
+    let resizeImgLarge = (fileName) => {
+        let shortName = fileName.slice(0, fileName.length - 4);
+        fs.readFileSync(`${fileName}`)
+            sharp(`${fileName}`, { failOnError: false })
+                .resize(1024, 768)
+                .toFile(`${shortName}-large.jpg`);
+    };
+    // resizeImgLarge("./images/shop-plants.jpg");
+
 };
