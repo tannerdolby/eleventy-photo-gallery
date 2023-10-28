a5b5f5357347e0e24dd71d0f33caf9136c0b249c
Tanner <tannercdolby@gmail.com>
2023-10-27 22:14:39 -0700

    feat: migrate to eleventy-img

--- .eleventy.js
+++ .eleventy.js
@@ -1,73 +1,111 @@
 const CleanCSS = require("clean-css");
 const { minify } = require("terser");
 const metagen = require("eleventy-plugin-metagen");
-const respimg = require("eleventy-plugin-sharp-respimg");
 const eleventyNavigation = require("@11ty/eleventy-navigation");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = (eleventyConfig) => {
-   
-    eleventyConfig.addPlugin(metagen);
-    eleventyConfig.addPlugin(respimg);
-    eleventyConfig.addPlugin(eleventyNavigation);
-    
-    eleventyConfig.setTemplateFormats([
-        "md",
-        "njk"
-    ]);
 
-    markdownTemplateEngine: "njk";
+  eleventyConfig.addPlugin(metagen);
+  eleventyConfig.addPlugin(eleventyNavigation);
 
-    // Perform manual passthrough file copy to include directories in the build output _site
-    eleventyConfig.addPassthroughCopy("./src/images");
-    eleventyConfig.addPassthroughCopy("./src/photos");
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addPassthroughCopy("./src/js");
-    eleventyConfig.addPassthroughCopy("./src/favicon_data");
+  eleventyConfig.setTemplateFormats([
+    "md",
+    "njk"
+  ]);
 
-    // Create css-clean CSS Minifier filter
-    eleventyConfig.addFilter("cssmin", function(code) {
-        return new CleanCSS({}).minify(code).styles;
-    });
+  markdownTemplateEngine: "njk";
 
-    // Create terser JS Minifier async filter (Nunjucks)
-    eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
-        code,
-        callback
-    )   {
-        try {
-            const minified = await minify(code);
-            callback(null, minified.code);
-        } catch (err) {
-            console.log(`Terser error: ${err}`);
-            // Fail gracefully
-            callback(null, code);
-        }
-    });
+  // Perform manual passthrough file copy to include directories in the build output _site
+  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/photos");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/favicon_data");
 
-    // Configure image in a template paired shortcode
-    eleventyConfig.addPairedShortcode("image", (srcSet, src, alt, sizes="(min-width: 400px) 33.3vw, 100vw") => {
-        return `<img srcset="${srcSet}" src="${src}" alt="${alt}" sizes="${sizes}" />`;
-    });
+  // Create css-clean CSS Minifier filter
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
-    // Configure outgoing Pexels anchor elements in a template paried shortcode
-    eleventyConfig.addPairedShortcode("link", (href, cls="image-link", rel="noopener", target="_blank", btnTxt="Pexels") => {
-        return `<a class="${cls}" href="${href}" rel="${rel}" target="${target}">${btnTxt}</a>`;
-    });
-
-    // get the current year to be placed in the footer
-    eleventyConfig.addShortcode("getYear", function() {
-        const year = new Date().getFullYear();
-        return `${year}`;
-    });
-    
-    return {
-        dir: {
-            input: "src",
-            output: "_site",
-            layouts: "_includes/layouts",
-            includes: "_includes",
-        },
-        templateFormats: ["md", "liquid", "njk"],
-        passthroughFileCopy: true
+  // Create terser JS Minifier async filter (Nunjucks)
+  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
+    code,
+    callback
+  ) {
+    try {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } catch (err) {
+      console.log(`Terser error: ${err}`);
+      callback(null, code);
     }
+  });
+
+  // Configure image in a template paired shortcode
+  eleventyConfig.addPairedShortcode("image", (srcSet, src, alt, sizes = "(min-width: 400px) 33.3vw, 100vw") => {
+    return `<img srcset="${srcSet}" src="${src}" alt="${alt}" sizes="${sizes}" />`;
+  });
+
+  // Configure outgoing Pexels anchor elements in a template paried shortcode
+  eleventyConfig.addPairedShortcode("link", (href, cls = "image-link", rel = "noopener", target = "_blank", btnTxt = "Pexels") => {
+    return `<a class="${cls}" href="${href}" rel="${rel}" target="${target}">${btnTxt}</a>`;
+  });
+
+  // get the current year to be placed in the footer
+  eleventyConfig.addShortcode("getYear", function () {
+    const year = new Date().getFullYear();
+    return `${year}`;
+  });
+
+  eleventyConfig.addShortcode("img", async function ({ src, alt, width, height, widths, className, imgDir, sizes = "100vw"}) {
+    if (alt === undefined) {
+      throw new Error(`Missing \`alt\` on responsive image from: ${src}`);
+    }
+
+    const IMAGE_DIR = imgDir || "./src/images/";
+    const metadata = await Image(IMAGE_DIR + src, {
+      widths: widths || [300, 480, 640, 1024],
+      formats: ["webp", "jpeg"],
+      urlPath: "/img/",
+      outputDir: "_site/img",
+      defaultAttributes: {
+        loading: "lazy",
+        decoding: "async"
+      }
+    });
+
+    let lowsrc = metadata.jpeg[0];
+    let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+    const sources = `${Object.values(metadata).map(
+      imageFormat => `<source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`
+    ).join("\n")}`;
+
+    const img = `
+      <img
+        src="${lowsrc.url}"
+        width="${highsrc.width}"
+        height="${highsrc.height}"
+        alt="${alt}"
+        loading="lazy"
+        decoding="async"
+        class="${className || ''}"
+        width="${width || ''}"
+        height="${height || ''}"
+      >`;
+
+    return `<picture>\n\t${sources}\n\t${img}</picture>`;
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      layouts: "_includes/layouts",
+      includes: "_includes",
+    },
+    templateFormats: ["md", "liquid", "njk"],
+    passthroughFileCopy: true
+  }
 };
